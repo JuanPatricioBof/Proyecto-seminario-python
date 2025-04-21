f3d16ca739bd2957fe3bc380a8b4ef2585083429
Facundo Carbone <carbonefacu78@gmail.com>
2025-04-21 20:22:22 -0300

    se agrego la columna material_techumbre correspondiente al ej 8

--- src/dataset/colums_hogar.py
+++ src/dataset/colums_hogar.py
@@ -31,5 +31,29 @@ def generate_column_tipo_hogar(path_hogar):
   print("✅ Se agregó la columna tipo_hogar con valores traducidos.")
     
 
+def generate_column_material_techumbre(path_hogar):
+#Se debe generar una nueva columna llamada MATERIAL_TECHUMBRE que indica el tipo de hogar basado en el campo V4:
+#- 5 a 7: "Material precario".
+#- 1 a 4: "Material durable".
+# 9: “No aplica”.
+  with path_hogar.open('r',encoding='utf-8')as file_csv:
+    reader=csv.DictReader(file_csv,delimiter=';')
+    fieldnames=reader.fieldnames
 
-   
+    #Se agrega la nueva columna
+    if('material_techumbre') not in fieldnames:
+        fieldnames.append('material_techumbre')
+    filas=[]
+    for row in reader:
+        if row['V4'] in ['5','6','7']:
+            row['material_techumbre']='Material precario'
+        elif row['V4'] in ['1','2','3','4']:
+            row['material_techumbre']='Material durable'
+        elif row['V4']=='9':
+            row['material_techumbre']='No aplica'
+        filas.append(row)
+  with path_hogar.open('w', newline = "", encoding='utf-8')as file_csv:
+        writer = csv.DictWriter(file_csv, fieldnames=fieldnames, delimiter=';')
+        writer.writeheader()
+        writer.writerows(filas)
+  print("✅ Se agregó la columna material_techumbre con valores traducidos.")
