c1f56ea3b91fd283e9d6096055bca252cf89fb77
Catalina Brochero <catabrochero18@gmail.com>
2025-04-30 16:44:28 -0300

    Ajuste PEP8 y cambio del uso de la funcion exit por condicional en colums_individuo.py

--- src/dataset/colums_hogar.py
+++ src/dataset/colums_hogar.py
@@ -1,47 +1,48 @@
 import csv
 
 def generar_columna_densidad_hogar(path_copia_hogar):
-    """ Genera una nueva columna denominada DENSIDAD_HOGAR según el siguiente 
-        criterio:
+    """ Genera una nueva columna denominada DENSIDAD_HOGAR según el
+        siguiente criterio:
 
         Bajo: menos de 1 persona por habitación.
         Medio: entre 1 y 2 personas por habitación.
         Alto: más de 2 personas por habitación.
         Desconocido: información insuficiente.
 
-        Para la generación se utilizan las columnas IX_TOT, que indica la cantidad
-        total de miembros del hogar, e IV2, que indica la cantidad de habitaciones
-        de la vivienda (sin contar baño/s, cocina, pasillo/s, lavadero, garage).
-        En caso de faltar la información de alguna de las columnas o estar 
-        mal cargadas, la nueva columna se marca como 'Desconocido'.
+        Para la generación se utilizan las columnas IX_TOT, que indica
+        la cantidad total de miembros del hogar, e IV2, que indica la
+        cantidad de habitaciones de la vivienda (sin contar baño/s,
+        cocina, pasillo/s, lavadero, garage). En caso de faltar la
+        información de alguna de las columnas o estar mal cargadas, la
+        nueva columna se marca como 'Desconocido'.
 
         En caso de ya existir, la columna se actualiza.
 """
     try:
         with open(path_copia_hogar, "r", encoding="utf-8") as file:
             try:
-                # guardo el encabezado y una lista con las filas
+                # Guardo el encabezado y una lista con las filas
                 csv_reader = csv.DictReader(file, delimiter=";")
                 header = csv_reader.fieldnames
                 rows = list(csv_reader)
             except StopIteration:
                 print(f"Error: Archivo vacío")
-    except FileNotFoundError:        
+    except FileNotFoundError:
         print(f"Error: archivo no encontrado")
     except PermissionError:
         print(f"Error: acceso al archivo denegado")
-    else: 
-        # actualizo el encabezado en caso de ser necesario
+    else:
+        # Actualizo el encabezado en caso de ser necesario
         if not "DENSIDAD_HOGAR" in header:
             header.append("DENSIDAD_HOGAR")
         print(header)
-        # compruebo que existan las columnas necesarias
+        # Compruebo que existan las columnas necesarias
         if not {"IX_TOT","IV2"}.issubset(header):
                 print(f"Error: falta una o más columnas para el procesamiento"
                       " de los datos")
-        else: 
-            # actualizo las filas calculando la densidad según 
-            # la cantidad de miembros y habitaciones
+        else:
+            # Actualizo las filas calculando la densidad según
+            # La cantidad de miembros y habitaciones
             for row in rows:
                 miembros = row["IX_TOT"]
                 habitaciones = row["IV2"]
@@ -52,14 +53,14 @@ def generar_columna_densidad_hogar(path_copia_hogar):
                         row["DENSIDAD_HOGAR"] = "Bajo"
                     elif(miembros <= habitaciones*2):
                         row["DENSIDAD_HOGAR"] = "Medio"
-                    else: 
-                        # más de dos miembros por habitación
+                    else:
+                        # Más de dos miembros por habitación
                         row["DENSIDAD_HOGAR"] = "Alto"
                 else:
-                    # faltan datos o están mal cargados
+                    # Faltan datos o están mal cargados
                     row["DENSIDAD_HOGAR"] = "Desconocido"
 
-        # sobreescirbo el archivo con los datos actualizados
+        # Sobreescirbo el archivo con los datos actualizados
         try:
             with open(path_copia_hogar, "w", newline = "") as file:
                 csv_writer = csv.DictWriter(file, fieldnames=header, delimiter=";")
--- src/dataset/colums_individuo.py
+++ src/dataset/colums_individuo.py
@@ -1,22 +1,29 @@
-#AJUSTAR PEP 8
 import csv
 
 def generar_universitario_completo(path_copia_individual):
-    """ genera una nueva columna llamada UNIVERSITARIO numérica que indica si
-    una persona mayor de edad ha completado el como mínimo el nivel 
-    universitario. Si la columna ya existe, actualiza los datos de la misma.
-    Los datos de la columna se representan de la siguiente manera:
-    "1": Sí.
-    "0": No.
-    "2": No aplica.
-    "": Indefinido (sin información suficiente).
-    Se considera que la persona ha completado el nivel universitario si:
-    - Su nivel educativo está registrado como universitario completo (columna
-    "NIVEL_ED")
-    ó
-    - Cursó por última vez el nivel universitario o superior. En caso de haber 
-    cursado por último el nivel universitario, completó ese nivel. (Columnas
-    "CH12" y "CH13")
+    """ genera una nueva columna llamada UNIVERSITARIO numérica que
+    indica si una persona mayor de edad ha completado, como mínimo, el
+    nivel universitario. 
+    
+    Los valores de la columna son:
+    - "1": Sí.
+    - "0": No.
+    - "2": No aplica.
+    - "": Indefinido (sin información suficiente).
+
+    Para la generación se utilizan las columnas "NIVEL_ED", "CH12" y
+    "CH13".
+
+    Se considera que la persona completó el nivel universitario si
+    cumple al menos una de las siguientes condiciones:
+    - Su nivel educativo está registrado como universitario completo
+    (valor 6 en la columna "NIVEL_ED").
+    - Cursó por última vez el nivel universitario o superior (valor
+    7 u 8 en la columna "CH12"), y, en caso de haber cursado por último
+    el nivel universitario, completó ese nivel. (valor 7 en la columna
+     "CH12" y valor 1 en la columna "CH13").
+
+    Si la columna ya existe, actualiza los datos de la misma.
     """
 
     # accedo al encabezado y a las filas del archivo
@@ -28,46 +35,43 @@ def generar_universitario_completo(path_copia_individual):
                 rows = list(csv_reader)
             except StopIteration:
                 print(f"Error: Archivo vacío.")
-                exit(1)
     except FileNotFoundError:
         print(f"Error: El archivo no se encontró")
-        exit(1)
     except PermissionError:
-        print(f"Error: Incapaz de acceder al archivo")
-        exit(1)
-
-    #actualizo el encabezado 
-    if not "UNIVERSITARIO" in header:
-        header.append("UNIVERSITARIO")
-
-    
-    # compruebo que el archivo contenga las columnas necesarias
-    if not {"CHO6", "CH12", "NIVEL_ED"}.issubset(header):
-        print(f"Error: no se encontraron una o más columnas necesarias"
-                " para el procesamiento")
+        print(f"Error: Acceso de lectura denegado")
     else:
-        # actualizo las filas
-        for row in rows:
-            edad = row["CH06"]
-            if(edad.isnumeric):
-                if(int(edad) >= 18):
-                    if((row["CH12"] =="8" or (row["CH12"] =="7" and 
-                    row["CH13"] == "1")) or row["NIVEL_ED"] == "6"):
-                        row["UNIVERSITARIO"] = "1" # sí
+        #actualizo el encabezado
+        if not "UNIVERSITARIO" in header:
+            header.append("UNIVERSITARIO")
+
+        # compruebo que el archivo contenga las columnas necesarias
+        if not {"CHO6", "CH12", "NIVEL_ED"}.issubset(header):
+            print(f"Error: no se encontraron una o más columnas necesarias"
+                    " para el procesamiento")
+        else:
+            # actualizo las filas
+            for row in rows:
+                edad = row["CH06"]
+                if(edad.isnumeric):
+                    if(int(edad) >= 18):
+                        if(row["NIVEL_ED"] == "6" or (row["CH12"] == "8" or
+                          (row["CH12"] == "7" and row["CH13"] == "1"))):
+                            row["UNIVERSITARIO"] = "1"  # sí
+                        else:
+                            row["UNIVERSITARIO"] = "0"  # no
                     else:
-                        row["UNIVERSITARIO"] = "0" # no
+                        row["UNIVERSITARIO"] = "2"  # no aplica
                 else:
-                    row["UNIVERSITARIO"] = "2" # no aplica 
-            else:
-                row["UNIVERSITARIO"] = "" # indefinido
+                    row["UNIVERSITARIO"] = "" # indefinido
 
-    # sobreescribo el archivo con los cambios
-    try: 
-        with open(path_copia_individual, "w", newline = "") as file:
-            csv_writer = csv.DictWriter(file,fieldnames=header,delimiter=";")
-            csv_writer.writeheader()
-            csv_writer.writerows(rows)
-    except PermissionError:
-        print(f"Error. El archivo no puede ser sobreeescrito")
-    else:
-        print(f"Columna 'UNIVERSITARIO_COMPLETO' generada")
+        # sobreescribo el archivo con los cambios
+        try:
+            with open(path_copia_individual, "w", newline = "") as file:
+                csv_writer = csv.DictWriter(file, fieldnames=header,
+                                            delimiter=";")
+                csv_writer.writeheader()
+                csv_writer.writerows(rows)
+        except PermissionError:
+            print(f"Error. El archivo no puede ser sobreeescrito")
+        else:
+            print(f"Columna 'UNIVERSITARIO_COMPLETO' generada")
