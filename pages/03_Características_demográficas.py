a2848b30fc19ae66c6099d9998099246f20e09ef
maius02 <maiapaez02@gmail.com>
2025-06-17 23:39:28 -0300

    mejora en la carga del/los dataframe/s a cada pagina

--- pages/03_Características_demográficas.py
+++ pages/03_Características_demográficas.py
@@ -5,47 +5,67 @@ from src.utils.loader import cargar_parcial_csv, cargar_json, file_exists
 from src.functions_streamlit.demografia import filtrar_individuos, agrupar_por_decada_y_genero, graficar_barras_dobles
 from src.utils.constants import PATHS
 
-# Configuración de la página
+# --- Configuración de la página ---
 st.set_page_config(
     page_title="Demografía",      # Cambialo según la página
     #page_icon="📊",               # Podés usar emojis distintos en cada página
     layout="wide",
     initial_sidebar_state="expanded"
 )
+# CASO PARA UNA SOLA ENCUESTA
+# --- Carga segura ---
 
-if file_exists(PATHS["individual"]["csv"]) and file_exists(PATHS["individual"]["json"]):
-    df_ind = cargar_parcial_csv(PATHS["individual"]["csv"], ['ANO4','TRIMESTRE','PONDERA','CH06','CH04_str']) # DataFrame individuos
-    fechas_ind = cargar_json(PATHS["individual"]["json"]) # Json individuos
-else:
-    st.warning("⚠️ Aún no se han generado los datos de la encuesta individual.\nPor favor, completá la página 2 primero.")
+columnas_necesarias = ['ANO4','TRIMESTRE','PONDERA','CH06','CH04_str']
+df_ind = cargar_parcial_csv(PATHS["individual"]["csv"], columnas_necesarias)
 
-# if file_exists(PATHS["hogar"]["csv"]) and file_exists(PATHS["hogar"]["json"]):
-#     df_hog = cargar_parcial_csv(PATHS["hogar"]["csv"], ['PONDERA','ANO4','TRIMESTRE']) # DataFrame hogares
-#     fechas_hog = cargar_json(PATHS["hogar"]["json"]) # Json hogares
-# else:
-#     st.warning("⚠️ Aún no se han generado los datos de la encuesta hogar.\nPor favor, completá la página 2 primero.")
+if df_ind.empty:
+    st.warning("No hay datos disponibles.")
+    st.stop()
+
+fechas_ind = cargar_json(PATHS["individual"]["json"])
+
+# CASO PARA DOS ENCUESTAS
+
+# puede estar vacio o con datos, o talvez preguntar si existe la key y en caso que no, fechas=none
+fechas = st.session_state.fechas_correspondencia 
+
+columnas_necesarias = ['ANO4','TRIMESTRE','PONDERA','CH06','CH04_str']
+df_ind = cargar_parcial_csv(PATHS["individual"]["csv"], columnas_necesarias, fechas)
+
+columnas_necesarias = ['ANO4','TRIMESTRE','PONDERA']
+df_hog = cargar_parcial_csv(PATHS["hogar"]["csv"], columnas_necesarias, fechas)
+
+# DataFrames vacios
+if df_ind.empty or df_hog.empty:
+    st.warning("No hay datos disponibles.")
+    st.stop()
 
 # -------------------------
 # INTERFAZ DE USUARIO
 # -------------------------
 
-st.title("📊Características demográficas.")
-
 st.subheader("1.3.1 Exploración de la población según **edad y sexo** por año y trimestre.")
-#with st.expander("1.3.1 Exploración de la población según **edad y sexo** por año y trimestre."):
 
 col1, col2 = st.columns(2)
 with col1:
     año_selec = st.selectbox("Seleccioná un año", fechas_ind.keys())#     # Eligir año de las opciones disponibles
-
 with col2:
     trim_selec = st.selectbox("Seleccioná un trimestre", fechas_ind[año_selec]) # Eligir trimestre disponible de ese año
-
+    
 df_filtrado = filtrar_individuos(df_ind, año_selec, trim_selec)
 agrupado = agrupar_por_decada_y_genero(df_filtrado)
+
+# with st.sidebar:
+#     st.header("🎛️ Filtros")
+#     año_selec = st.selectbox("Seleccioná un año", fechas_ind.keys())
+#     trim_selec = st.selectbox("Seleccioná un trimestre", fechas_ind[año_selec])
+#     df_filtrado = filtrar_individuos(df_ind, año_selec, trim_selec)
+#     agrupado = agrupar_por_decada_y_genero(df_filtrado)
 
 fig = graficar_barras_dobles(agrupado, año_selec, trim_selec, colores= ["#FFB6C1", "#6495ED"] )
 st.pyplot(fig)
 
-with st.expander("1.3.2 Informar..."):
-    st.write("Proximamente...")
+st.divider()
+
+# --- Gráfico de distribución por sexo ---
+st.subheader("Otro punto")
--- src/utils/loader.py
+++ src/utils/loader.py
@@ -9,16 +9,32 @@ from src.utils.constants import DATA_OUT_PATH, PATHS
 # -------------------------
 
 @st.cache_data
-def cargar_parcial_csv(path, usecols=None):
-    """
-    Lee un archivo CSV y lo retorna como DataFrame de pandas parcial.
+def cargar_parcial_csv(path, usecols=None, filtro=None):
+    """Lee un archivo CSV y lo retorna como DataFrame de pandas parcial.
+
     Args:
-        path (str or Path): Ruta del archivo CSV.
-        usecols (list): lista de columnas a guardar
+        path (str or Path): Ruta al archivo CSV
+        usecols (list), optional): Columnas a guardar. Defaults to None.
+        filtro (list(tuples), optional): Años y trimestres a filtrar. Defaults to None.
+
     Returns:
-        pd.DataFrame: DataFrame con los datos cargados.
+        pd.DataFrame: DataFrame con los datos cargados.    
     """
-    return pd.read_csv(path, sep=";", decimal=",", low_memory=False, usecols=usecols)
+    if not os.path.exists(path):
+        st.warning(f"⚠️ El archivo {path} no existe.")
+        return pd.DataFrame() # Vacío
+    
+    df = pd.read_csv(path, sep=";", decimal=",", low_memory=False, usecols=usecols)
+    
+    if filtro:
+        df = df[df[["año", "trimestre"]].apply(tuple, axis=1).isin(filtro)]
+    # Explicación de como funciona
+    # df[["año", "trimestre"]] #Devuelve un DataFrame con solo esas 2 columnas.
+    # .apply(tuple, axis=1) Convierte cada fila en una tupla.
+    # .isin(filtros) Devuelve una serie booleana que dice si cada tupla está o no en la lista de filtros: [True, False, True, False]
+    # df[ ... ] Aplica ese filtro y se queda solo con las filas True.
+
+    return df
 
 
 @st.cache_data
@@ -36,16 +52,16 @@ def cargar_json(path):
     return {int(k): [int(tri) for tri in v] for k, v in fechas.items()}
 
 
-def file_exists(path):
-    """Verifica que el archivo exista
+# def file_exists(path):
+#     """Verifica que el archivo exista
 
-    Args:
-        path (path or str): ruta al archivo
+#     Args:
+#         path (path or str): ruta al archivo
 
-    Returns:
-        boolean: True si existe, false si no.
-    """    
-    return os.path.exists(path)
+#     Returns:
+#         boolean: True si existe, false si no.
+#     """    
+#     return os.path.exists(path)
 
 
 # def cargar_datos_en_session():
