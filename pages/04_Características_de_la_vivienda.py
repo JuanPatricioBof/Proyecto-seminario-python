7df6e9169d558b4218e7bb5f412f703127637697
Catalina Brochero <cata.brochero18@gmail.com>
2025-06-18 16:45:26 -0300

    incisos 1 y 2

--- pages/04_Características_de_la_vivienda.py
+++ pages/04_Características_de_la_vivienda.py
@@ -1,32 +1,54 @@
 import sys
 import os
-
-# Agrega "code/src" al sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
-
-from utils.constants import PATHS
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Agrega "code/src" al sys.path
+sys.path.append('..')
+
+from src.utils.constants import PATHS
+from src.utils.loader import cargar_parcial_csv, cargar_json
+from src.functions_streamlit.vivienda import hogares_encuestados
+from src.functions_streamlit.vivienda import mostrar_grafico_torta
 
 
 # carga de dataframe
 columnas=['ANO4','TRIMESTRE','AGLOMERADO','PONDERA','IV1','IV1_ESP','IV3','IV3_ESP',
             'IV8','IV9','IV12_3','II7','II7_ESP','CONDICION_DE_HABITABILIDAD'] 
 
-df_viviendas = cargar_parcial_csv(PATHS['hogar']['csv'],columnas)
-json_fechas = cargar_json(PATHS['hogar'['json']])
+#df_viviendas = cargar_parcial_csv(PATHS['hogar']['csv'],columnas)
+#json_fechas = cargar_json(PATHS['hogar'['json']])
 
-# seleccionar un año
+df_viviendas = pd.read_csv(PATHS['hogar']['csv'],sep=';',usecols=columnas)
+json_fechas = pd.read_json(PATHS['hogar']['json'])
+
+# lista de años
 opciones = [anio for anio in json_fechas]
 opciones.append('Mostrar para todos los años')
 
 # Interfaz 
 st.title("Características de la vivienda.")
-op = st.selectbox("Elegir año",opciones,index=len(opciones)-1)
 
-# filtrado 
+# filtrado (modularizar?)
+
+op = st.selectbox("Elegir año",opciones,index=len(opciones)-1,key='anio_p3')
 if(op != 'Mostrar para todos los años'):
-   df_filtrado = df_viviendas[df_viviendas['ANO4']==op]
+    df_filtrado = df_viviendas[df_viviendas['ANO4']==op]
 else:
    df_filtrado = df_viviendas
 
+# Inciso 1
+total_encuestados = hogares_encuestados(df_filtrado['PONDERA'])
+st.write(f' - Cantidad de hogares encuestados🏠: {total_encuestados}')
+
+# Inciso 2
+st.subheader('Distribución según tipos de hogar en Argentina')
+mostrar_grafico_torta(df_filtrado,total_encuestados)
+
+# Inciso 3
+#informar_piso_dominante_por_aglomerado(df_filtrado)
+
+# Inciso 4
+
+# Inciso 5
--- src/functions_streamlit/vivienda.py
+++ src/functions_streamlit/vivienda.py
@@ -1,1 +1,66 @@
-"""Pagina 03. Funciones de análisis de vivienda"""
+"""Pagina 04. Funciones de análisis de vivienda"""
+import sys
+import os
+sys.path.append('..')
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+from src.utils.constants import diccionario_aglomerados
+
+def hogares_encuestados(ponderacion_hogares):
+        return ponderacion_hogares.sum()
+
+def categoria(tipo):
+    #1. casa
+    #2. departamento
+    #3. pieza de inquilinato
+    #4. pieza en hotel / pensión
+    #5. local no construido para habitación
+    match int(tipo):
+          case 1:
+                return 'casa'
+          case 2:
+                return 'departamento'
+          case 3:
+                return 'pieza de inquilinato'
+          case 4:
+                return 'pieza en hotel/pensión'
+          case 5:
+                return 'local no construido para habitación'
+          case 6: 
+                return 'otros'
+          case _:
+                return 'indefinido' 
+
+def mostrar_grafico_torta(df_filtrado, total_encuestados):
+    
+    tipos_viviendas = df_filtrado.groupby('IV1')['PONDERA'].sum()      
+    
+    etiquetas = [f'{categoria(tipo)} ({(valor/total_encuestados):0.1%})' for tipo, valor in tipos_viviendas.items()]
+    
+    figura, ejex = plt.subplots(figsize=(5,3))
+    
+    ejex.pie(
+        tipos_viviendas,
+        autopct=None,
+        labels=None
+    )
+
+    ejex.legend(
+        title = 'Tipo de vivienda',
+        labels = etiquetas,
+        bbox_to_anchor=(1, 0.5), 
+        loc="center left"
+    )
+
+    ejex.set_ylabel('')
+    ejex.set_title='Distribución de tipo de viviendas en Argentina'
+
+    st.pyplot(figura)
+
+def informar_piso_dominante_por_aglomerado(df_filtrado):
+      df_filtrado.groupby(['AGLOMERADO','IV3'])['PONDERA'].sum()
+
+
+
